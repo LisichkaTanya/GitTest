d103f2da1a8357c8bbae2ed9483217d9031f7322
Tatiana Solomatina <lisichka-tanya@mail.ru>
2020-10-13 19:48:51 +0300

    delete two message

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.println("How are you?");
-        System.out.println("come to play!");
+
     }
 }
